56d3729a184e010165103e965f9e9764f4dd011e
ok <zhangji@hotmail.com>
2017-08-29 21:27:28 +0800

    add content.

--- app/src/main/java/org/soundofhope/ad/RecyclerViewAdapter.java
+++ app/src/main/java/org/soundofhope/ad/RecyclerViewAdapter.java
@@ -1,17 +1,32 @@
 package org.soundofhope.ad;
 
+import android.app.Activity;
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdLoader;
 import com.google.android.gms.ads.NativeExpressAdView;
+import com.google.android.gms.ads.VideoController;
+import com.google.android.gms.ads.doubleclick.PublisherAdRequest;
+import com.google.android.gms.ads.formats.MediaView;
+import com.google.android.gms.ads.formats.NativeAd;
+import com.google.android.gms.ads.formats.NativeAppInstallAd;
+import com.google.android.gms.ads.formats.NativeAppInstallAdView;
+import com.google.android.gms.ads.formats.NativeContentAd;
 import com.google.android.gms.ads.formats.NativeContentAdView;
 
 import java.util.List;
+import java.util.Locale;
 
 /**
  * The {@link RecyclerViewAdapter} class.
@@ -25,12 +40,16 @@ class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> 
     // The Native  ad view type.
     private static final int NATIVE_CONTENT_AD_VIEW_TYPE = 1;
 
+    private static final String DFP_AD_UNIT_ID = "/6499/example/native";
+
+    private static final String SIMPLE_TEMPLATE_ID = "10104090";
     // An Activity's Context.
     private final Context mContext;
 
     // The list of Native Express ads and menu items.
     private final List<Object> mRecyclerViewItems;
 
+    final static StringBuffer headLine = new StringBuffer();
     /**
      * For this example app, the recyclerViewItems list contains only
      * {@link MenuItem} and {@link NativeExpressAdView} types.
@@ -38,6 +57,38 @@ class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> 
     public RecyclerViewAdapter(Context context, List<Object> recyclerViewItems) {
         this.mContext = context;
         this.mRecyclerViewItems = recyclerViewItems;
+
+        this.initContent();
+    }
+
+    private void initContent() {
+
+        AdLoader.Builder builder = new AdLoader.Builder( mContext, DFP_AD_UNIT_ID);
+
+        builder.forContentAd(new NativeContentAd.OnContentAdLoadedListener() {
+            @Override
+            public void onContentAdLoaded(NativeContentAd ad) {
+
+                NativeContentAdView adView = (NativeContentAdView) ((Activity)mContext).getLayoutInflater()
+                        .inflate(R.layout.ad_content, null);
+                //populateContentAdView(ad, adView);
+                System.out.println( "11111111111" + ad.getHeadline() );
+
+                headLine.append( ad.getHeadline() );
+            }
+        });
+
+        AdLoader adLoader = builder.withAdListener(new AdListener() {
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                System.out.println("Failed to load native ad: ");
+
+                Toast.makeText(mContext, "Failed to load native ad: "
+                        + errorCode, Toast.LENGTH_SHORT).show();
+            }
+        }).build();
+
+        adLoader.loadAd(new PublisherAdRequest.Builder().build());
     }
 
     /**
@@ -57,11 +108,19 @@ class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> 
 
     public class NativeContentAdViewHolder extends RecyclerView.ViewHolder {
 
-        private TextView menuHeadlineView;
+        private TextView HeadlineView;
+        private TextView BodyView;
+        private TextView CallToActionView;
+        private TextView AdvertiserView;
+
 
         NativeContentAdViewHolder(View view) {
             super(view);
-            menuHeadlineView = (TextView) view.findViewById(R.id.contentad_headline);
+
+            HeadlineView = (TextView) view.findViewById(R.id.contentad_headline);
+            BodyView = (TextView) view.findViewById(R.id.contentad_body);
+            CallToActionView = (TextView) view.findViewById(R.id.contentad_call_to_action);
+            AdvertiserView = (TextView) view.findViewById(R.id.contentad_advertiser);
         }
     }
 
@@ -115,6 +174,51 @@ class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> 
 
     }
 
+
+    /**
+     * Populates a {@link NativeContentAdView} object with data from a given
+     * {@link NativeContentAd}.
+     *
+     * @param nativeContentAd the object containing the ad's assets
+     * @param adView          the view to be populated
+     */
+    private void populateContentAdView(NativeContentAd nativeContentAd,
+                                       NativeContentAdView adView) {
+        adView.setHeadlineView(adView.findViewById(R.id.contentad_headline));
+        adView.setBodyView(adView.findViewById(R.id.contentad_body));
+        adView.setCallToActionView(adView.findViewById(R.id.contentad_call_to_action));
+        adView.setLogoView(adView.findViewById(R.id.contentad_logo));
+        adView.setAdvertiserView(adView.findViewById(R.id.contentad_advertiser));
+        // The MediaView will display a video asset if one is present in the ad, and the first image
+        // asset otherwise.
+        adView.setMediaView((MediaView) adView.findViewById(R.id.contentad_media));
+
+        // Some assets are guaranteed to be in every NativeContentAd.
+        ((TextView) adView.getHeadlineView()).setText(nativeContentAd.getHeadline());
+        ((TextView) adView.getBodyView()).setText(nativeContentAd.getBody());
+        ((TextView) adView.getCallToActionView()).setText(nativeContentAd.getCallToAction());
+        ((TextView) adView.getAdvertiserView()).setText(nativeContentAd.getAdvertiser());
+
+        // These assets aren't guaranteed to be in every NativeContentAd, so it's important to
+        // check before trying to display them.
+        NativeAd.Image logoImage = nativeContentAd.getLogo();
+
+        if (logoImage == null) {
+            adView.getLogoView().setVisibility(View.INVISIBLE);
+        } else {
+            ((ImageView) adView.getLogoView()).setImageDrawable(logoImage.getDrawable());
+            adView.getLogoView().setVisibility(View.VISIBLE);
+        }
+
+        // Assign native ad object to the native view.
+        adView.setNativeAd(nativeContentAd);
+
+        System.out.println( nativeContentAd.getHeadline() );
+        System.out.println( nativeContentAd.getBody() );
+        System.out.println( nativeContentAd.getCallToAction() );
+        System.out.println( nativeContentAd.getAdvertiser() );
+    }
+
     /**
      *  Replaces the content in the views that make up the menu item view and the
      *  Native Express ad view. This method is invoked by the layout manager.
@@ -132,10 +236,14 @@ class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> 
                 break;
 
             case NATIVE_CONTENT_AD_VIEW_TYPE:
+
                 NativeContentAdViewHolder nativeContentAdViewHolder = (NativeContentAdViewHolder) holder;
+
+                System.out.println( "2222222" + headLine.toString() );
+                nativeContentAdViewHolder.HeadlineView.setText( headLine.toString() );
                 //MenuItem menuItem = (MenuItem) mRecyclerViewItems.get(position);
                 // fall through\
-                nativeContentAdViewHolder.menuHeadlineView.setText( "abc" );
+                //nativeContentAdViewHolder.menuHeadlineView.setText( "abc" );
                 break;
             default:
 
